48c75c531b59e6213f7f709765837bc4d2ece4e6
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2024-05-22 18:13:00 -0300

    refactor: Implement lazy loading for images in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,7 @@
+import { motion } from 'framer-motion'
+import Link from 'next/link'
+import { useEffect, useRef, useState } from 'react'
+
 const navLinks = [
   { name: 'Home', href: '#home' },
   { name: 'Projetos', href: '#projects' },
@@ -11,20 +15,40 @@ interface NavLink {
 }
 
 export function NavBar({ activeSection }: NavLink) {
+  const [activeStyles, setActiveStyles] = useState({ left: 0, width: 0 })
+  const navRefs = useRef<Array<HTMLLIElement | null>>([])
+
+  useEffect(() => {
+    const index = navLinks.findIndex(
+      (link) => link.href.slice(1) === activeSection,
+    )
+    if (index !== -1 && navRefs.current[index]) {
+      const { offsetLeft, offsetWidth } = navRefs.current[index] ?? {}
+      setActiveStyles({ left: offsetLeft ?? 0, width: offsetWidth ?? 0 })
+    }
+  }, [activeSection])
+
   return (
-    <nav className="flex m-auto items-center justify-between p-1 bg-white rounded-full w-fit">
-      <ul className="flex items-center space-x-4">
-        {navLinks.map((link) => (
+    <nav className="relative flex m-auto items-center justify-between p-1 bg-white rounded-full w-fit">
+      <motion.div
+        className="absolute top-0 bottom-0 m-1 rounded-full bg-black"
+        initial={false}
+        animate={activeStyles}
+        transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+      />
+      <ul className="flex items-center space-x-3 relative">
+        {navLinks.map((link, index) => (
           <li
             key={link.href}
-            className={`py-2 px-4 rounded-full ${activeSection === link.href.slice(1) ? 'text-zinc-300 bg-black' : 'hover:text-zinc-300'}`}
+            ref={(el: never) => (navRefs.current[index] = el)}
+            className={`relative z-10 py-2 px-4 rounded-full ${activeSection === link.href.slice(1) && 'text-white'}`}
           >
-            <a
+            <Link
               href={link.href}
-              className={`font-light ${activeSection === link.href.slice(1) ? 'text-white' : 'text-zinc-500'}`}
+              className={`${activeSection === link.href.slice(1) ? 'text-white font-bold' : 'text-zinc-500 font-light'}`}
             >
               {link.name}
-            </a>
+            </Link>
           </li>
         ))}
       </ul>
